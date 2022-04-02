eb7900423ab44c53ed1cff5ceec4dd46639aca0d
lee.wp14 <lee.wp14@gmail.com>
2022-04-02 16:32:32 +0800

    config.mirui: add some initial config
    Define default topup conversion rate and movie cost.

--- config/mirui.php
+++ config/mirui.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Facade;
 
 return [
-
+    'standards' => [
+        'coins' => [
+            'myr_equivalent' => 5,
+        ],
+        'movie' => [
+            'coins' => 5,
+        ],
+    ],
 ];
