f2d698478a3e72fcc88c463363186319401260e3
tung <yentung0730@gmail.com>
2022-04-03 00:31:26 +0800

    Creadit Card Validator

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Facades\Validator;
+use Carbon\Carbon;
 
 class Transaction extends Model
 {
@@ -44,7 +46,8 @@ class Transaction extends Model
             },
             //in: from user input
             set: function($value){
-                // dump($value);
+                dump($value);
+                dump(self::cardNumValidator(['cardnumber' => $value]));
                 $return = intval(filter_var($value, FILTER_SANITIZE_NUMBER_INT));
                 // dump($return);
                 return $return;
@@ -62,7 +65,9 @@ class Transaction extends Model
             },
             //in: from user input
             set: function($value){
-                // dump($value);
+                dump($value);
+                dump(self::expValidator(['expiration' => $value]));
+                
                 $input = intval(filter_var($value, FILTER_SANITIZE_NUMBER_INT));
                 // dump($return);
                 $count=0;
@@ -86,5 +91,80 @@ class Transaction extends Model
                 return $return;
             }
         );
+    }
+
+    /**
+     * Security Class
+     * 
+     * PHP Library: composer.phar require jlorente/php-credit-cards
+     * 
+     * https://stripe.com/docs/testing#cards
+     */
+
+    //public function amountValidator($amount){
+    //    $validatedAmount = Validator::make(
+    //        ['amount' => $amount],
+    //        ['amount' => 'required|integer|max:500|min:10'],
+    //        ['required' => 'Amount is required!'],
+    //    );
+    //}
+
+    public function cardNumValidator($cardnumber){
+        $return = [];
+
+        //$intCardNum = intval(trim($cardnumber['cardnumber'], ' '));
+        
+        $intCardNum = intval(str_replace(' ', '', $cardnumber['cardnumber']));
+        //dump($intCardNum);
+
+        $validatedCardNum = Validator::make(
+            ['intCardNum' => $intCardNum],
+            ['intCardNum' => 'min:1000000000000000|max:9999999999999999'],
+            //['intCardNum' => 'required|regex:/[0-9]{4}\s[0-9]{4}\s[0-9]{4}\s[0-9]{4}$/'],
+            ['min' => 'Invalid card number!', 'max' => 'Invalid card number!'], //, 'regex' => 'Card Number must have 16 numbers!'
+        );
+        if($validatedCardNum->fails()){
+            return $validatedCardNum; 
+        }
+
+        return $return;
+    }
+
+    public function expValidator($expiration){
+        $return = [];
+
+        if (strlen($expiration['expiration']) == 5){
+            $seperator = explode('/', $expiration['expiration']);
+            $month = intval($seperator[0]);
+            $year = intval($seperator[1]) + 2000;
+
+            $validatedExp = Validator::make(
+                ['month' => $month, 'year' => $year],
+                ['month' => 'integer|max:12|min:1', 'year' => 'integer'],
+                ['integer' => 'Only integer is acceptable!', 'min' => 'Minimum month is 1!', 'max' => 'Maximum month is 12!'],                                   
+            );
+            if($validatedExp->fails()){
+                return $validatedExp; 
+            }
+            //Carbon
+            $monthYear = Carbon::now();
+            $monthYear->month = $month;
+            $monthYear->year = $year;
+
+            if($monthYear->lessThan(Carbon::now())){
+                $return = ['expiration' => 'Expiration is invalid!'];
+            }
+        }
+        return $return;
+    }
+
+    public function cvvValidator($cvv){
+        $return = [];
+        $validatedCvv = Validator::make(
+            ['cvv' => $cvv],
+            ['cvv' => 'required|digits:3|min:001|max:999'],
+            ['required' => 'CVV is required!', 'digits' => 'Only 3 digits for CVV!'],
+        );
+        return $validatedCvv;
     }
 }
